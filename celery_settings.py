ee0b6263fad8e9381347c75916b6f5423ba44053
thisisharrison <harrison.lau@outlook.com>
2020-07-17 21:52:16 +0800

    fix

--- celery_settings.py
+++ celery_settings.py
@@ -1,13 +1,24 @@
 import os
 import redis
 
-## Broker settings.
-broker_url = os.environ.get('REDIS_URL','redis://localhost:6379/0')
+# ## Broker settings.
+# broker_url = os.environ['REDIS_URL']
 
-# # List of modules to import when the Celery worker starts.
-# imports = ('myapp.tasks',)
+# # # List of modules to import when the Celery worker starts.
+# # imports = ('myapp.tasks',)
 
-# ## Using the database to store task state and results.
-result_backend = 'REDIS_URL'
+# # ## Using the database to store task state and results.
+# celery_result_backend = os.environ['REDIS_URL']
 
-# task_annotations = {'tasks.add': {'rate_limit': '10/s'}}
+# # task_annotations = {'tasks.add': {'rate_limit': '10/s'}}
+
+# celery_task_serializer = 'json'
+
+broker_url = os.environ['REDIS_URL']
+result_backend = os.environ['REDIS_URL']
+
+task_serializer = 'json'
+result_serializer = 'json'
+accept_content = ['json']
+timezone = 'Europe/Oslo'
+enable_utc = True
