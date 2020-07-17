20ca83bb9bc8425ed6b7b678231791100afd34e8
thisisharrison <harrison.lau@outlook.com>
2020-07-17 23:45:07 +0800

    templates ready

--- app/main.py
+++ app/main.py
@@ -6,7 +6,27 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     hello.delay()
-    # return "<h1>Successful</h1>"
-    return render_template('new_index.html')
-    # return render_template('index.html')
-    # return redirect(url_for('index'))
+    return render_template('index.html')
+
+@app.route("/image", methods=["GET", "POST"])
+def image():
+    return render_template('image.html')
+
+
+@app.route("/image/<task_id>")
+def image_result(task_id):
+    pass
+
+
+@app.route("/pdp", methods=["GET", "POST"])
+def prodpdp():
+    return render_template('pdp.html')
+
+
+@app.route("/pdp/<task_id>")
+def pdp_result(task_id):
+    pass
+
+@app.route("/cdp", methods=["GET", "POST"])
+def prodcdp():
+    return render_template('cdp.html')
